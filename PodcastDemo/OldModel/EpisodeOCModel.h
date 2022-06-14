505fe76f2070cb40caf269070d88c720648ef0d4
leo <a841223o@gmail.com>
2022-06-15 02:40:48 +0800

    EpisodeParser implement

--- PodcastDemo/OldModel/EpisodeOCModel.h
+++ PodcastDemo/OldModel/EpisodeOCModel.h
@@ -8,8 +8,26 @@
 #import <Foundation/Foundation.h>
 
 @class EpisodeOCModel ;
+@class EpisodeOCItem ;
+
 
 @interface EpisodeOCModel : NSObject
+@property  (nonatomic, copy) NSString *title;
 @property  (nonatomic, copy) NSString *headImageUrl;
+@property  (nonatomic, copy) NSArray<EpisodeOCItem *> *items;
+@end
+
+
+@interface EpisodeOCItem : NSObject
+
+@property  (nonatomic, copy) NSString *title;
+@property  (nonatomic, copy) NSString *date;
+@property  (nonatomic, copy) NSString *summary;
+@property  (nonatomic, copy) NSString *image;
+@property  (nonatomic, copy) NSString *url;
+@property  (nonatomic, copy) NSString *type;
+@property  (nonatomic, copy) NSString *length;
+- (instancetype)initWithDictionary:(NSDictionary*)dictionary;
 
 @end
+
