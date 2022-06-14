7198f556757f202348c0476c6ee94eb174715a6e
leo <a841223o@gmail.com>
2022-06-14 19:36:40 +0800

    add episoodeMode

---
+++ PodcastDemo/OldModel/EpisodeOCModel.h
@@ -0,0 +1,15 @@
+//
+//  EpisodeOCModel.h
+//  PodcastDemo
+//
+//  Created by leo on 2022/6/14.
+//
+
+#import <Foundation/Foundation.h>
+
+@class EpisodeOCModel ;
+
+@interface EpisodeOCModel : NSObject
+@property  (nonatomic, copy) NSString *headImageUrl;
+
+@end
---
+++ PodcastDemo/OldModel/EpisodeOCModel.m
@@ -0,0 +1,13 @@
+//
+//  EpisodeModelOC.m
+//  PodcastDemo
+//
+//  Created by leo on 2022/6/14.
+//
+
+#import "EpisodeOCModel.h"
+
+@implementation EpisodeOCModel
+@end
+
+
